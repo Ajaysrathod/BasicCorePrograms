2d0c452e34e76be1fdb11ae96db99117e81f8344
ajayrr29@gmail.com <ajayrr29@gmail.com>
2023-05-02 09:15:48 +0530

    [Ajay]-Welcome to Leap Year

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -5,13 +5,17 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Basic Core Program");
-            Console.WriteLine("1:Flip coin");
+            Console.WriteLine("1:Flip coinn\n2:Leap Year");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
                 case 1:
                     FlipCoinUC1 flipCoin = new FlipCoinUC1();
                     flipCoin.FlipCoins();
+                    break;
+                case 2:
+                    LeapYearUC2 leapYear = new LeapYearUC2();
+                    leapYear.LeapYears();
                     break;
                 default:
                     Console.WriteLine("Please enter right option");
