62c107626ee1ca828e8d9b400136380e88c2cc2f
ajayrr29@gmail.com <ajayrr29@gmail.com>
2023-05-02 09:34:51 +0530

    [Ajay]-Welcome to Harmonic Series

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Basic Core Program");
-            Console.WriteLine("1:Flip coinn\n2:Leap Year\n3:Power of two");
+            Console.WriteLine("1:Flip coinn\n2:Leap Year\n3:Power of two\n4:Harmonics");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -20,6 +20,10 @@
                 case 3:
                     PowerOfTwoUC3 powerOfTwo = new PowerOfTwoUC3();
                     powerOfTwo.PowerOf2();
+                    break;
+                case 4:
+                    HarmonicNumberUC4 harmonics = new HarmonicNumberUC4();
+                    harmonics.HarmonicsSeries();
                     break;
                 default:
                     Console.WriteLine("Please enter right option");
