0b4e380d453daa7ceb64c01748bd6424a3cac543
ajayrr29@gmail.com <ajayrr29@gmail.com>
2023-05-02 09:44:19 +0530

    [Ajay]-Welcome to Factors

--- BasicCorePrograms/Program.cs
+++ BasicCorePrograms/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Basic Core Program");
-            Console.WriteLine("1:Flip coinn\n2:Leap Year\n3:Power of two\n4:Harmonics");
+            Console.WriteLine("1:Flip coinn\n2:Leap Year\n3:Power of two\n4:Harmonics\n5:Factors");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -24,6 +24,10 @@
                 case 4:
                     HarmonicNumberUC4 harmonics = new HarmonicNumberUC4();
                     harmonics.HarmonicsSeries();
+                    break;
+                case 5:
+                    FactorsUC5 factors = new FactorsUC5();
+                    factors.FactorsNumber();
                     break;
                 default:
                     Console.WriteLine("Please enter right option");
